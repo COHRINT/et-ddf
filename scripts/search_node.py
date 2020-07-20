d0d262b9f677e1ee4df1062b9ca0c7346ea27695
Luke Morrissey <luke.morrissey@colorado.edu>
2020-07-20 13:10:44 -0600

    Added changes

--- scripts/search_node.py
+++ scripts/search_node.py
@@ -41,9 +41,8 @@ class Volume:
         self.x_range = [vol_dict['volume']['x1'],vol_dict['volume']['x2']]
         self.y_range = [vol_dict['volume']['y1'],vol_dict['volume']['y2']]
         self.z = vol_dict['volume']['z']
-        
 
-class Search:
+class Space:
     def __init__(self,v):
         self.volume = v
         x_diff = v.x_range[1]-v.x_range[0]
@@ -52,42 +51,6 @@ class Search:
         if y_diff > x_diff:
             self.range = y_diff/4.
         self.discritize_space()
-
-        #Make sure both rovs are armed
-        rospy.wait_for_service('/bluerov2_3/uuv_control/arm_control')
-        arm_control_3 = rospy.ServiceProxy('/bluerov2_3/uuv_control/arm_control', ArmControl)
-        resp1 = arm_control_3()
-
-        rospy.wait_for_service('/bluerov2_4/uuv_control/arm_control')
-        arm_control_4 = rospy.ServiceProxy('/bluerov2_4/uuv_control/arm_control', ArmControl)
-        resp1 = arm_control_4()    
-        
-        #Set up services to set the velocity
-        rospy.wait_for_service('/bluerov2_3/uuv_control/set_heading_velocity')
-        self.shv_3 = rospy.ServiceProxy('/bluerov2_3/uuv_control/set_heading_velocity', SetHeadingVelocity)
-
-        rospy.wait_for_service('/bluerov2_4/uuv_control/set_heading_velocity')
-        self.shv_4 = rospy.ServiceProxy('/bluerov2_4/uuv_control/set_heading_velocity', SetHeadingVelocity)
-
-        #Set up services to set the sonar settings
-        rospy.wait_for_service('/bluerov2_3/set_sonar_settings')
-        self.set_sonar_3 = rospy.ServiceProxy('/bluerov2_3/set_sonar_settings',SetSonarSettings)
-
-        rospy.wait_for_service('/bluerov2_3/set_sonar_settings')
-        self.set_sonar_4 = rospy.ServiceProxy('/bluerov2_4/set_sonar_settings',SetSonarSettings)
-
-        #Subscribes to the necisary topics for the search node for each rov
-        rospy.Subscriber("/bluerov2_3/etddf/estimate/bluerov2_3",Odometry,self.ownship_callback_3)
-        rospy.Subscriber("/bluerov2_4/etddf/estimate/bluerov2_4",Odometry,self.ownship_callback_4)
-        rospy.Subscriber("/bluerov2_3/ping_360_target",SonarTargetList,self.detections_3)
-        rospy.Subscriber("/bluerov2_4/ping_360_target",SonarTargetList,self.detections_4)
-        rospy.Subscriber("/bluerov2_3/pose_gt",Odometry,self.truth_callback_3)
-        rospy.Subscriber("/bluerov2_4/pose_gt",Odometry,self.truth_callback_4)
-        rospy.Subscriber("/bluerov2_3/visual_pub",LaserScan,self.angle_callback_3)
-        rospy.Subscriber("/bluerov2_4/visual_pub",LaserScan,self.angle_callback_4)
-        self.truth_yaw_3 = None
-        self.truth_yaw_4 = None
-
     def discritize_space(self):
         self.x_diff = self.volume.x_range[1] - self.volume.x_range[0]
         self.y_diff = self.volume.y_range[1] - self.volume.y_range[0]
@@ -97,18 +60,55 @@ class Search:
         self.space = np.zeros((x_blocks,y_blocks))
         self.x_cords = [self.volume.x_range[0] + 0.25*i for i in range(x_blocks)]
         self.y_cords = [self.volume.y_range[0] + 0.25*i for i in range(y_blocks)]
-    def ownship_callback_3(self,msg):
-        self.position_3 = msg.pose.pose.position
-        (r,p,self.yaw_3) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-    def ownship_callback_4(self,msg):
-        self.position_4 = msg.pose.pose.position
-        (r,p,self.yaw_4) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-    def inview_3(self,x,y,angles):
-        x_diff = x-self.truth_pos_3.x
-        y_diff = y-self.truth_pos_3.y
+        
+        
+
+class Search:
+    def __init__(self,space,name):
+        #name in format '/bluerov2_X'
+        self.space = space
+        self.name = name
+
+        #Make sure both rovs are armed
+        rospy.wait_for_service(self.name+'/uuv_control/arm_control')
+        arm_control = rospy.ServiceProxy(self.name+'/uuv_control/arm_control', ArmControl)
+        resp1 = arm_control()
+    
+        
+        #Set up services to set the velocity
+        rospy.wait_for_service(self.name+'/uuv_control/set_heading_velocity')
+        self.shv = rospy.ServiceProxy(self.name+'/uuv_control/set_heading_velocity', SetHeadingVelocity)
+
+
+        #360 sonar settings with 10 m range
+        settings = SonarSettings()
+        settings.range = 10
+        settings.min_angle = -4
+        settings.max_angle = 4
+
+        #Set up services to set the sonar settings
+        rospy.wait_for_service(self.name+'/set_sonar_settings')
+        self.set_sonar = rospy.ServiceProxy(self.name+'/set_sonar_settings',SetSonarSettings)
+
+        self.set_sonar(settings)
+
+        #Subscribes to the necisary topics for the search node for each rov
+        rospy.Subscriber(self.name+"/etddf/estimate/bluerov2_3",Odometry,self.ownship_callback)
+        rospy.Subscriber(self.name+"/bluerov2_3/ping_360_target",SonarTargetList,self.detections)
+        rospy.Subscriber(self.name+"/bluerov2_3/pose_gt",Odometry,self.truth_callback)
+        rospy.Subscriber(self.name+"/bluerov2_3/visual_pub",LaserScan,self.angle_callback)
+        self.truth_yaw = None
+
+    def ownship_callback(self,msg):
+        self.position = msg.pose.pose.position
+        (r,p,self.yaw) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+    
+    def inview(self,x,y,angles):
+        x_diff = x-self.truth_pos.x
+        y_diff = y-self.truth_pos.y
         dist = np.linalg.norm([x_diff,y_diff])
         xy_ang = np.arctan2(y_diff,x_diff)
-        xy_ang -= self.truth_yaw_3
+        xy_ang -= self.truth_yaw
         xy_ang = normalize_angle(xy_ang)
         # print(xy_ang)
         # print(angles)
@@ -118,21 +118,8 @@ class Search:
         if dist < self.range:
             return True
         return False
-    
-    def inview_4(self,x,y,angles):
-        x_diff = x-self.truth_pos_4.x
-        y_diff = y-self.truth_pos_4.y
-        dist = np.linalg.norm([x_diff,y_diff])
-        xy_ang = np.arctan2(y_diff,x_diff)
-        xy_ang -= self.truth_yaw_4
-        xy_ang = normalize_angle(xy_ang)
-        if xy_ang-.1 > angles[-1] or xy_ang+.1 < angles[0]:
-            return False
-        if dist < self.range:
-            return True
-        return False
 
-    def angle_callback_3(self,msg):
+    def angle_callback(self,msg):
         #read in from the visual publisher to know where it is looking
         #determine the angle range it has viewed
         angle_increment = msg.angle_increment
@@ -141,73 +128,31 @@ class Search:
         for i in range(len(msg.ranges)):
             if msg.ranges[i]!=-1:
                 current_angles.append(angles[i])
-        self.not_seen_3(current_angles)
-    def angle_callback_4(self,msg):
-        #read in from the visual publisher to know where it is looking
-        #determine the angle range it has viewed
-        angle_increment = msg.angle_increment
-        angles = [-np.pi+angle_increment*i for i in range(len(msg.ranges))]
-        current_angles = []
-        for i in range(len(msg.ranges)):
-            if msg.ranges[i]!=-1:
-                current_angles.append(angles[i])
-        self.not_seen_4([-3.14,3.14])
-    def not_seen_3(self,angles):
+        self.not_seen(current_angles)
+
+    def not_seen(self,angles):
         for i in range(len(self.x_cords)):
             for j in range(len(self.y_cords)):
                 if self.space[i][j] <= 0.01:
-                    if self.inview_3(self.x_cords[i],self.y_cords[j],angles):
+                    if self.inview(self.x_cords[i],self.y_cords[j],angles):
                         self.space[i][j]=-1
 
-    def not_seen_4(self,angles):
-        for i in range(len(self.x_cords)):
-            for j in range(len(self.y_cords)):
-                if self.space[i][j] <= 0.01:
-                    if self.inview_4(self.x_cords[i],self.y_cords[j],angles):
-                        self.space[i][j]=-1
 
-    def detections_3(self,msg):
+    def detections(self,msg):
         found = False
         for target in msg.targets:
             if target.id != 'red_asset':
                 continue
             else:
                 found = True
-                self.spotted_3(target)
-    def detections_4(self,msg):
-        found = False
-        for target in msg.targets:
-            if target.id != 'red_asset':
-                continue
-            else:
-                found = True
-                self.spotted_4(target)
-    def spotted_3(self,target):
+                self.spotted(target)
+    def spotted(self,target):
         range_to = target.range_m
         bearing = target.bearing_rad
-        bearing += self.truth_yaw_3
+        bearing += self.truth_yaw
         bearing = normalize_angle(bearing)
-        x = self.truth_pos_3.x + range_to*np.cos(bearing)
-        y = self.truth_pos_3.y + range_to*np.sin(bearing)
-        x_close = None
-        y_close = None
-        for i in range(len(self.x_cords)):
-            if np.norm(self.x_cords[i]-x) < .25:
-                x_close = i
-                break
-        for i in range(len(self.y_cords)):
-            if np.norm(self.y_cords[i]-y) < .25:
-                y_close = i
-                break
-        self.space[x_close][y_close] = 1
-    
-    def spotted_4(self,target):
-        range_to = target.range_m
-        bearing = target.bearing_rad
-        bearing += self.truth_yaw_3
-        bearing = normalize_angle(bearing)
-        x = self.truth_pos_3.x + range_to*np.cos(bearing)
-        y = self.truth_pos_3.y + range_to*np.sin(bearing)
+        x = self.truth_pos.x + range_to*np.cos(bearing)
+        y = self.truth_pos.y + range_to*np.sin(bearing)
         x_close = None
         y_close = None
         for i in range(len(self.x_cords)):
@@ -220,12 +165,9 @@ class Search:
                 break
         self.space[x_close][y_close] = 1
 
-    def truth_callback_3(self,msg):
-        self.truth_pos_3 = msg.pose.pose.position
-        (r,p,self.truth_yaw_3) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-    def truth_callback_4(self,msg):
-        self.truth_pos_4 = msg.pose.pose.position
-        (r,p,self.truth_yaw_4) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+    def truth_callback(self,msg):
+        self.truth_pos = msg.pose.pose.position
+        (r,p,self.truth_yaw) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
     def update(self):
         self.space *= .95
     def run(self):
@@ -283,8 +225,11 @@ if __name__ == "__main__":
     with open('hello.yaml') as file:
         volume = yaml.load(file, Loader=yaml.FullLoader)
     v = Volume(volume)
-    s = Search(v)
-    s.run()
+    space = Space(v)
+    s3 = Search(space,'/bluerov2_3')
+    s4 = Search(space,'/bluerov2_4')
+    s3.run()
+    s4.run()
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
         rate.sleep()
