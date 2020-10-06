52ba914f574dc27a9048a5808abfcc78249b8ad3
Robert Glissmann <evan.sneath@orbitlogic.com>
2020-10-06 18:56:26 +0000

    Added setpoint utilitiy scripts. Fixed some other issues.

--- scripts/quat2euler.py
+++ scripts/quat2euler.py
@@ -5,14 +5,21 @@ import rospy
 import tf
 from nav_msgs.msg import Odometry
 import numpy as np
+from sensor_msgs.msg import Imu
 
 def callback(msg):
     ori = msg.pose.pose.orientation
     roll, pitch, yaw = tf.transformations.euler_from_quaternion( (ori.x, ori.y, ori.z, ori.w) )
     print(yaw * (180 / np.pi))
+def imu_callback(imu_msg):
+    # ori = msg.pose.pose.orientation
+    roll, pitch, yaw = tf.transformations.euler_from_quaternion([imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w])
+    # roll, pitch, yaw = tf.transformations.euler_from_quaternion( (ori.x, ori.y, ori.z, ori.w) )
+    print(yaw * (180 / np.pi))
 
 rospy.init_node("quat2euler")
 
-rospy.Subscriber("/bluerov2_4/odometry/filtered", Odometry, callback)
+# rospy.Subscriber("bluerov2_3/odometry/filtered", Odometry, callback)
+rospy.Subscriber("bluerov2_3/mavros/imu/data/corrected", Imu, imu_callback)
 
 rospy.spin()
